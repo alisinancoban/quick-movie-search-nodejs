fda4d83656e07b9f7df4a224eeb54c68fc0a4951
Ali Sinan Çoban <alisinancoban@gmail.com>
2022-08-17 16:14:00 +0300

    connection string change

--- util/database.js
+++ util/database.js
@@ -5,7 +5,11 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://alisinancoban:2281832@appdb.nq5cyqj.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://alisinancoban:2281832@appdb.nq5cyqj.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+       useUnifiedTopology: true
+    }
   )
     .then(client => {
       console.log('Connected!');
